07df62056fa0b1c9f3d5e9162bf6b5ca353f11f4
Marco Torre <marco.a.torre@gmail.com>
2021-01-15 15:19:55 -0500

    Updated Node#parent

--- aA-classwork/W3D5/node/lib/00_tree_node.rb
+++ aA-classwork/W3D5/node/lib/00_tree_node.rb
@@ -1,10 +1,18 @@
-class PolyTreeNode
-    attr_reader :parent, :children
-    attr_writer :val
+require "byebug"
 
-    def initialize(val)
-        @val = val
+class PolyTreeNode
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @value = value
         @parent = nil
         @children = []
     end
+
+    def parent=(node)
+        @parent = value
+        @parent.children << child_a
+        # @children = node.first
+    end
+
 end
