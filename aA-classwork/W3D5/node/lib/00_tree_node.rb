bb8514c6ac5dd2527fca21bc6b6edef7d0dd7b0b
yonatan zonszein <yonatanzonszein@gmail.com>
2021-01-15 14:20:30 -0500

    building initialize

--- aA-classwork/W3D5/node/lib/00_tree_node.rb
+++ aA-classwork/W3D5/node/lib/00_tree_node.rb
@@ -1,3 +1,10 @@
 class PolyTreeNode
+    attr_reader :parent, :children
+    attr_writer :val
 
+    def initialize(val)
+        @val = val
+        @parent = nil
+        @children = []
+    end
 end
